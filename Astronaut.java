3c2c3f8709ff9b6aab9e12f2d94533be116e960b
Epix1080 <66178958+Epix1080@users.noreply.github.com>
2020-05-30 17:17:49 -0500

    Update Astronaut.java

--- Astronaut.java
+++ Astronaut.java
@@ -12,27 +12,28 @@ public class Astronaut {
 	int feeling;
 	int hobby;
 	int daysOnMission;
-	
 	double mentalHealth;
 	double physicalHealth;
+	double mentalHealthCalc;
+	double physicalHealthCalc;
 	double health;
 	
-	public Astronaut(String name, double mentalHealth, double physicalHealth, int feeling) {
+	public Astronaut(String name, int exercise, int nutrition, int sleep, int hydration, int feeling, int wounded, int interaction, int hobby) {
 		this.name=name;
-		this.mentalHealth=mentalHealth;
-		this.physicalHealth=physicalHealth;
-		this.health=((mentalHealth+physicalHealth)/2;
+		this.physicalHealth=calculatePhysicalHealth(exercise, nutrition, sleep, hydration, feeling, wounded)*10;
+		this.mentalHealth=calculateMentalHealth(interaction, feeling, hobby, calculatePhysicalHealth(exercise, nutrition, sleep, hydration, feeling, wounded))*10;
+		this.health=(this.mentalHealth+this.physicalHealth)/2;
 		this.feeling= feeling;
 	}
 	
-	public double calculatephysicalHealth(int exercise, int nutrition, int sleep, int hydration, int feeling, int wounded) {
-		physicalHealth=exercise*.15+nutrition*.20+sleep*.15+hydration*.20+feeling*10+wounded*5;
-		return physicalHealth;
+	public double calculatePhysicalHealth(int exercise, int nutrition, int sleep, int hydration, int feeling, int wounded) {
+		physicalHealthCalc=exercise*.20+nutrition*.25+sleep*.20+hydration*.20+feeling*.10+ wounded*.05;
+		return physicalHealthCalc;
 	}
 	
 	public double calculateMentalHealth(int interaction, int feeling, int hobby, double physicalHealth){
-		mentalHealth= interaction*.25+feeling*.25+hobby*.25+physicalHealth*.15+ sleep*10;
-		return mentalHealth;
+		mentalHealthCalc= interaction*.25+feeling*.25+hobby*.25+physicalHealth*.15+ sleep*.10;
+		return mentalHealthCalc;
 	}
 
 }
