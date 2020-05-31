398aa3bb48ea2ae12a5243c4211f0a264239b229
aranis23 <58046249+aranis23@users.noreply.github.com>
2020-05-31 14:15:40 -0500

    Update Astronaut.java

--- Astronaut.java
+++ Astronaut.java
@@ -7,7 +7,7 @@ public class Astronaut {
 	int hydration;
 	int sleep;
 	int wounded;
-	
+
 	int interaction;
 	int feeling;
 	int hobby;
@@ -16,27 +16,32 @@ public class Astronaut {
 	static double physicalHealth;
 	double mentalHealthCalc;
 	double physicalHealthCalc;
-     static  double health;
-    static double result;
-	
-	public Astronaut(String name, int exercise, int nutrition, int sleep, int hydration, int feeling, int wounded, int interaction, int hobby) {
-		this.name=name;
-		this.physicalHealth=calculatePhysicalHealth(exercise, nutrition, sleep, hydration, feeling, wounded);
-		this.mentalHealth=calculateMentalHealth(interaction, feeling, hobby, calculatePhysicalHealth(exercise, nutrition, sleep, hydration, feeling, wounded),sleep);
-		this.health=(this.mentalHealth+this.physicalHealth)/2;
-		this.feeling= feeling;
+	static double health;
+	static double result;
+
+	public Astronaut(String name, int exercise, int nutrition, int sleep, int hydration, int feeling, int wounded,
+			int interaction, int hobby) {
+		this.name = name;
+		this.physicalHealth = calculatePhysicalHealth(exercise, nutrition, sleep, hydration, feeling, wounded);
+		this.mentalHealth = calculateMentalHealth(interaction, feeling, hobby,
+				calculatePhysicalHealth(exercise, nutrition, sleep, hydration, feeling, wounded), sleep);
+		this.health = (this.mentalHealth + this.physicalHealth) / 2;
+		this.feeling = feeling;
 	}
-	
-	public double calculatePhysicalHealth(int exercise, int nutrition, int sleep, int hydration, int feeling, int wounded) {
-		physicalHealthCalc=exercise*.20+nutrition*.25+sleep*.20+hydration*.20+feeling*.10+ wounded*.05;
+
+	public Astronaut() {
+	}
+
+	public double calculatePhysicalHealth(int exercise, int nutrition, int sleep, int hydration, int feeling,
+			int wounded) {
+		physicalHealthCalc = exercise * .20 + nutrition * .25 + sleep * .20 + hydration * .20 + feeling * .10
+				+ wounded * .05;
 		return physicalHealthCalc;
 	}
-	
-	public double calculateMentalHealth(int interaction, int feeling, int hobby, double physicalHealth, int sleep){
-		mentalHealthCalc= interaction*.25+feeling*.25+hobby*.25+physicalHealth*.15+ sleep*.10;
-		return mentalHealthCalc;
-    }
-    
 
+	public double calculateMentalHealth(int interaction, int feeling, int hobby, double physicalHealth, int sleep) {
+		mentalHealthCalc = interaction * .25 + feeling * .25 + hobby * .25 + physicalHealth * .15 + sleep * .10;
+		return mentalHealthCalc;
+	}
 
 }
