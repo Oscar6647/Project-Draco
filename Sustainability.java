74369b8e921054778bd50c5e5ecb362b67314d38
aranis23 <58046249+aranis23@users.noreply.github.com>
2020-05-31 14:16:21 -0500

    Update Sustainability.java

--- Sustainability.java
+++ Sustainability.java
@@ -1,8 +1,9 @@
-package Draco;
-
 public class Sustainability {
-	private int remainingMissionDays;
-	private double sustainability;
+	private static int remainingMissionDays;
+	static double sustainabilityShip, sustainabilityBase;
+	public static double foodP;
+	public static double sustainabilityBase2;
+	public static double foodDLeft, waterDLeft, oxygenDLeft, fuelDLeft;
 
 	public Sustainability(double foodPercentage, double waterPercentage, double oxygenPercentage, double fuelPercentage,
 			double dailyFoodUsage, double dailyWaterUsage, double dailyOxygenUsage, double dailyFuelUsage,
@@ -20,25 +21,18 @@ public class Sustainability {
 		data[1][2] = dailyOxygenUsage;
 		data[1][3] = dailyFuelUsage;
 
-		calculateSustainability(data);
+		foodDLeft = data[0][0]/data[1][0] ;
+		waterDLeft = data[0][1]/data[1][1];
+		oxygenDLeft = data[0][2]/data[1][2];
+		fuelDLeft = data[0][3]/data[1][3];
+
+	}
+	
+	static double calculateBaseSustainability(double foodPercentage, double waterPercentage, double oxygenPercentage, double fuelPercentage) {
+
+		sustainabilityBase= (foodPercentage * 0.25) + (waterPercentage * 0.25) + (oxygenPercentage* 0.40) + (fuelPercentage * 0.10);
+		
+		return sustainabilityBase * 100;
 	}
 
-	private double calculateShipSustainability(double[][] data) {
-		for (int i = 0; i < data[0].length; i++) {
-			data[2][i] = data[0][i] - (data[1][i] * remainingMissionDays);
-		}
-		sustainability = (data[2][0] * 0.25) + (data[2][1] * 0.25) + (data[2][2] * 0.40) + (data[2][3] * 0.10);
-		return sustainability * 100;
-	}
-	private double calculateBaseSustainability(double[][] data) {
-		for (int i = 0; i < data[0].length-1; i++) {
-			data[2][i] = data[0][i] - (data[1][i] * remainingMissionDays);
-		}
-		sustainability = (data[2][0] * 0.30) + (data[2][1] * 0.30) + (data[2][2] * 0.40);
-		return sustainability * 100;
-	}
-
-	public double getSustainability() {
-		return sustainability;
-	}
 }
