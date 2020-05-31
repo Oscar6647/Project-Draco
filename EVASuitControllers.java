7e5d9a5bc0afbfb94ea91b8e0b576011bc3c0a4d
maxprpxam <61604692+maxprpxam@users.noreply.github.com>
2020-05-31 00:56:07 -0500

    Create EVASuitControllers.java

---
+++ EVASuitControllers.java
@@ -0,0 +1,20 @@
+package Draco;
+
+public class EVASuitControllers {
+	private double dataSuit[];
+	private double oxygenSuit;
+	private double energySuit;
+	private double waterSuit;
+	
+	public EVASuitControllers(double oxygensuit, double energySuit, double waterSuit) {
+		dataSuit = new double[] {oxygenSuit, energySuit, waterSuit};
+	}
+	
+	public void status(double[] dataSuit) {
+		String out = "The EVA suit status is:" + "\n" + 
+				"Oxygen of the suit: " + dataSuit[0] + "\n" +
+				"Energy of the suit: " + dataSuit[1] + "\n" +
+				"Water of the suit: " + dataSuit[2];
+		System.out.println(out);
+	}
+}
