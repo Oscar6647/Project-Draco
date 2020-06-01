12f6f5125e7d28148b562ab7d896d222798cd232
maxprpxam <61604692+maxprpxam@users.noreply.github.com>
2020-05-31 21:18:09 -0500

    Update SpacecraftControllers.java

--- SpacecraftControllers.java
+++ SpacecraftControllers.java
@@ -3,47 +3,49 @@ package Draco;
 public class SpacecraftControllers {
 	private String controllerPosition;
 	private double[] dataSpacecraft;
-	private double oxygenSpacecraft;
-	private double energySpacecraft;
-	private double fuelSpacecraft;
-	private double waterSpacecraft;
 	private boolean abort;
 	
 	public SpacecraftControllers(String controllerPosition, double oxygenSpacecraft, double energySpacecraft, double fuelSpacecraft, double waterSpacecraft) {
-		this.oxygenSpacecraft = oxygenSpacecraft;
-		this.energySpacecraft = energySpacecraft;
-		this.fuelSpacecraft = fuelSpacecraft;
-		this.waterSpacecraft = waterSpacecraft;
 		dataSpacecraft = new double[] {oxygenSpacecraft, energySpacecraft, fuelSpacecraft, waterSpacecraft};
 		this.controllerPosition = controllerPosition;
 	}
 	
-	public void onRoute() {
+	public String onRoute() {
 		String out = "The spacecraft is now on route." + "\n" + 
 					"ControllerPosition: " + controllerPosition + "\n" + 
 					"Oxygen in the spacecraft: " + dataSpacecraft[0] + "\n" +
 					"Energy in the spacecraft: " + dataSpacecraft[1] + "\n" +
 					"Fuel in the spacecraft: " + dataSpacecraft[2] + "\n" +
 					"Water in the spacecraft: " + dataSpacecraft[3] + "\n";
-		System.out.println(out);
+		return out;
 	}
 	
-	public void lunarDescent() {
+	public String lunarDescent() {
 		String out = "The spacecraft is now descending on the moon." + "\n" + 
 				"ControllerPosition: " + controllerPosition + "\n" + 
 				"Oxygen in the spacecraft: " + dataSpacecraft[0] + "\n" +
 				"Energy in the spacecraft: " + dataSpacecraft[1] + "\n" +
 				"Fuel in the spacecraft: " + dataSpacecraft[2] + "\n" +
 				"Water in the spacecraft: " + dataSpacecraft[3] + "\n";
-		System.out.println(out);
+		return out;
 	}
 	
-	public boolean abort(boolean abort) {
+	public void setAbort(boolean abort) {
+		this.abort = abort;
+		abort(this.abort);
+	}
+
+	public String abort(boolean abort) {
+		String returnAbort;
 		if (abort == true) {
-			System.out.println("Mission aborted!");
-			return true;
-		} else {
-			return false;
+			returnAbort = "Abort Mission!";
+			return returnAbort;
+		} else { 
+			return "";
 		}
+	}
+
+	public double[] getDataSpacecraft() {
+		return dataSpacecraft;
 	}
 }
