da9234e07052a82b44b225221e6ca292e851c117
Epix1080 <66178958+Epix1080@users.noreply.github.com>
2020-05-31 20:53:38 -0500

    Update AstroSchedules.java

--- AstroSchedules.java
+++ AstroSchedules.java
@@ -1,8 +1,13 @@
+package Draco;
+
+import java.awt.Button;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JProgressBar;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -11,7 +16,7 @@ import javax.swing.JTextPane;
 
 public class AstroSchedules {
 
-	 JFrame frame;
+	 JPanel frame;
 	 JProgressBar MissionDay;
 
 	/**
@@ -44,17 +49,17 @@ public class AstroSchedules {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 521, 351);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frame.getRootPane().setLayout(null);
 		
 		MissionDay = new JProgressBar();
 		MissionDay.setValue(0);
 		MissionDay.setStringPainted(true);
 		MissionDay.setBounds(166, 11, 329, 14);
-		frame.getContentPane().add(MissionDay);
+		frame.getRootPane().add(MissionDay);
 		
 		JLabel lblMissionDayProgress = new JLabel("Mission Day Progress");
 		lblMissionDayProgress.setBounds(10, 11, 146, 14);
-		frame.getContentPane().add(lblMissionDayProgress);
+		frame.getRootPane().add(lblMissionDayProgress);
 		
 		JButton btnStart = new JButton("START");
 		btnStart.addActionListener(new ActionListener() {
@@ -64,22 +69,33 @@ public class AstroSchedules {
 			}
 		});
 		btnStart.setBounds(10, 36, 89, 23);
-		frame.getContentPane().add(btnStart);
+		frame.getRootPane().add(btnStart);
 		
 		JTextPane textPane = new JTextPane();
 		//displays schedule
 		textPane.setToolTipText("");
 		textPane.setBounds(20, 70, 475, 231);
-		frame.getContentPane().add(textPane);
+		frame.getRootPane().add(textPane);
 		
 		JLabel lblOverallStayProgress = new JLabel("Overall Stay Progress");
 		lblOverallStayProgress.setBounds(109, 36, 146, 14);
-		frame.getContentPane().add(lblOverallStayProgress);
+		frame.getRootPane().add(lblOverallStayProgress);
 		
 		JProgressBar Overrall = new JProgressBar();
 		Overrall.setValue(0);
 		Overrall.setStringPainted(true);
 		Overrall.setBounds(227, 36, 268, 14);
-		frame.getContentPane().add(Overrall);
+		frame.getRootPane().add(Overrall);
+		
+		JButton btnNewButton = new JButton("Start EVA");
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               
+                    JOptionPane.showInputDialog(e, Schedules.printStringArr(Schedules.getSchedule1()));
+              
+                }
+            });
+        
 	}
 }
+
